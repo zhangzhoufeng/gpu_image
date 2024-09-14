1e014e1f9f9ba832d4598577068455c4ce903f9c
zhangzhoufeng <zhoufeng.zhang@aokoshare.com>
2024-09-14 13:40:07 +0800

    注释camera类报错

--- android/src/main/kotlin/com/gstory/gpu_image/GpuImageViewPlugin.kt
+++ android/src/main/kotlin/com/gstory/gpu_image/GpuImageViewPlugin.kt
@@ -1,7 +1,6 @@
 package com.gstory.gpu_image
 
 import android.app.Activity
-import com.gstory.gpu_image.camera.CameraFactory
 import com.gstory.gpu_image.imageview.ImageViewFactory
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
@@ -14,7 +13,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 object GpuImageViewPlugin {
     fun registerWith(binding: FlutterPlugin.FlutterPluginBinding, activity : Activity) {
         //注册相机
-        binding.platformViewRegistry.registerViewFactory("com.gstory.gpu_image/camera", CameraFactory(binding.binaryMessenger,activity))
+//        binding.platformViewRegistry.registerViewFactory("com.gstory.gpu_image/camera", CameraFactory(binding.binaryMessenger,activity))
         //注册图片
         binding.platformViewRegistry.registerViewFactory("com.gstory.gpu_image/image", ImageViewFactory(binding.binaryMessenger,activity))
     }
